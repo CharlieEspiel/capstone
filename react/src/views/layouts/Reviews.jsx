a4ca316d1d91418fd13fcb83c95b6ae010f928b7
webdevanthonybalucan <129866396+webdevanthonybalucan@users.noreply.github.com>
2023-07-28 21:19:26 +0800

    updated the contents in testimonials

--- react/src/views/layouts/Reviews.jsx
+++ react/src/views/layouts/Reviews.jsx
@@ -6,7 +6,7 @@ import {
     CardFooter,
     Rating
 } from "@material-tailwind/react";
-import { AnimatePresence, motion } from "framer-motion";
+
 // page components
 
 export default function Reviews() {
@@ -22,7 +22,6 @@ export default function Reviews() {
                         as="span"
                         variant="h2"
                         className="inline-block font-black tracking-normal text-[#cf0029]"
-
                     >
                         1,457+ customers and clients
                     </Typography>
@@ -33,7 +32,7 @@ export default function Reviews() {
                 </Typography>
             </div>
             <div className="flex flex-col flex-wrap md:flex-row max-w-7xl mx-auto items-center justify-center h-max gap-5">
-            <Card className="group mt-6 max-w-[22rem] hover:bg-subscribe transition-all ease-in-out duration-300 hover:scale-105 cursor-pointer">
+                <Card className="group mt-6 max-w-[22rem] hover:bg-subscribe transition-all ease-in-out duration-300 hover:scale-105 cursor-pointer">
                     <CardHeader color="blue-gray" onBlur={100} className="relative h-full">
                         <img
                             src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
@@ -43,12 +42,30 @@ export default function Reviews() {
                     </CardHeader>
                     <CardBody className="group-hover:text-white">
                         <Typography variant="h5" color="blue-gray" className="mb-2 group-hover:text-white">
-                            UI/UX Review Check
+                            Fast Repair Status Tracking
                         </Typography>
                         <Typography>
-                            The place is close to Barceloneta Beach and bus stop just 2 min by
-                            walk and near to &quot;Naviglio&quot; where you can enjoy the main
-                            night life in Barcelona.
+                            I was impressed with how quickly I could track the status of my electronic device repair. The online tracking system made it easy to know exactly when my device was ready for pickup.
+                        </Typography>
+                    </CardBody>
+                    <CardFooter className="pt-0">
+                        <Rating value={5} readonly unratedColor="red" ratedColor="red" />
+                    </CardFooter>
+                </Card>
+                <Card className="group mt-6 max-w-[22rem] hover:bg-subscribe transition-all ease-in-out duration-300 hover:scale-105 cursor-pointer">
+                    <CardHeader color="blue-gray" onBlur={100} className="relative h-full">
+                        <img
+                            src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
+                            alt="card-image"
+                            className="object-cover"
+                        />
+                    </CardHeader>
+                    <CardBody className="group-hover:text-white">
+                        <Typography variant="h5" color="blue-gray" className="mb-2 group-hover:text-white">
+                            Excellent Customer Service
+                        </Typography>
+                        <Typography>
+                            The customer service team was very helpful throughout the repair process. They provided regular updates and made sure I could easily track the status of my electronic device repair.
                         </Typography>
                     </CardBody>
                     <CardFooter className="pt-0">
@@ -65,38 +82,14 @@ export default function Reviews() {
                     </CardHeader>
                     <CardBody className="group-hover:text-white">
                         <Typography variant="h5" color="blue-gray" className="mb-2 group-hover:text-white">
-                            UI/UX Review Check
+                            Hassle-Free Repair Process
                         </Typography>
                         <Typography>
-                            The place is close to Barceloneta Beach and bus stop just 2 min by
-                            walk and near to &quot;Naviglio&quot; where you can enjoy the main
-                            night life in Barcelona.
+                            Tracking the status of my electronic device repair was so convenient. I knew exactly when it was ready, and the pickup process was smooth and hassle-free. Glad that I have discovered this kind of services online.
                         </Typography>
                     </CardBody>
                     <CardFooter className="pt-0">
-                        <Rating value={4} readonly unratedColor="red" ratedColor="red" />
-                    </CardFooter>
-                </Card>
-                <Card className="group mt-6 max-w-[22rem] hover:bg-subscribe transition-all ease-in-out duration-300 hover:scale-105 cursor-pointer">
-                    <CardHeader color="blue-gray" onBlur={100} className="relative h-full">
-                        <img
-                            src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
-                            alt="card-image"
-                            className="object-cover"
-                        />
-                    </CardHeader>
-                    <CardBody className="group-hover:text-white">
-                        <Typography variant="h5" color="blue-gray" className="mb-2 group-hover:text-white">
-                            UI/UX Review Check
-                        </Typography>
-                        <Typography>
-                            The place is close to Barceloneta Beach and bus stop just 2 min by
-                            walk and near to &quot;Naviglio&quot; where you can enjoy the main
-                            night life in Barcelona.
-                        </Typography>
-                    </CardBody>
-                    <CardFooter className="pt-0">
-                        <Rating value={4} readonly unratedColor="red" ratedColor="red" />
+                        <Rating value={5} readonly unratedColor="red" ratedColor="red" />
                     </CardFooter>
                 </Card>
             </div>
